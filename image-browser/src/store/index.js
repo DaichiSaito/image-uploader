12cce37f90bfd987f0a403086d0a3926bae02def
Thom David <thomdavid74@gmail.com>
2018-08-14 08:39:41 -0700

    L88 Instantiating Vuex

---
+++ image-browser/src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex' // access to vuex
+import Vue from 'vue' // required to access vuex
+
+Vue.use(Vuex) // allows Vue and Vuex to communicate
+
+export default new Vuex.Store({
+  modules: {}
+}) 
