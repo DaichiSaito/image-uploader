45d2ddfa0c5cf024474a7ce679c0871857273513
Thom David <thomdavid74@gmail.com>
2018-08-14 09:00:03 -0700

    L89 Oauth module instantiated

--- image-browser/src/main.js
+++ image-browser/src/main.js
@@ -3,6 +3,6 @@ import App from './App'
 import store from './store'
 
 new Vue({
-  store: store,  // connects the Vuex data
+  store,  // connects the Vuex data; can also be written as store: store,
   render: h => h(App)
 }).$mount(('#app'))
