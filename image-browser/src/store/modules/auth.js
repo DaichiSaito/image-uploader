38a08fb5a9c533a31f8062babf605b400127b3c0
Thom David <thomdavid74@gmail.com>
2018-08-14 10:44:56 -0700

    L91 Auth state, getters

---
+++ image-browser/src/store/modules/auth.js
@@ -0,0 +1,16 @@
+const state = {
+  token: null // initial value of token at page load
+}
+
+const getters = {
+  isLoggedIn: state => !!state.token // change 'state' to a boolean; returns true if not null
+}
+
+const actions = {
+
+
+}
+
+const mutations = {
+
+}
