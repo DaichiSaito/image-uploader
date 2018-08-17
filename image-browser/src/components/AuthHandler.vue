a5878288b90ffaa74d99228b39fbb44ef4e6b90c
Thom David <thomdavid74@gmail.com>
2018-08-17 13:59:08 -0700

    L104 Lifecycle action for finalizeLogin

--- image-browser/src/components/AuthHandler.vue
+++ image-browser/src/components/AuthHandler.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'AuthHandler'
+  name: 'AuthHandler',
+  methods: mapActions(['finalizeLogin']),
+  created() { 
+    // Vue lifecycle method
+    this.finalizeLogin(window.location.hash)
+  }
 }
 </script>
