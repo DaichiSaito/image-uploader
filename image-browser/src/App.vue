2e1a1780876aea74eca546f3efcc0d6b2a5dfffe
Thom David <thomdavid74@gmail.com>
2018-08-17 09:53:57 -0700

    L102 Vue router testing

--- image-browser/src/App.vue
+++ image-browser/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <AppHeader></AppHeader>
+    <router-view></router-view>
   </div>
 </template>
 
--- image-browser/src/main.js
+++ image-browser/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router' // get vue router library
 import App from './App'
 import store from './store'
 import AuthHandler from './components/AuthHandler'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter) // tell vue to use the vue router
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'history', // removes hash in url
   routes: [
     { path: '/oauth2/callback', component: AuthHandler }
   ]
@@ -16,5 +16,5 @@ const router = new VueRouter({
 new Vue({
   router, // registers the VueRouter; can be written router: router,
   store,  // registers the Vuex data; can also be written as store: store,
-  render: h => h(App)
+  render: h => h(App) // the render method
 }).$mount(('#app'))
