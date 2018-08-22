e934d3154a26a0c753a704bbeda21b3e5afa9dc2
Thom David <thomdavid74@gmail.com>
2018-08-22 15:32:40 -0700

    L135/136 Style gallery, suppress images at logout

--- image-browser/src/components/ImageList.vue
+++ image-browser/src/components/ImageList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <img v-for="image in allImages" :src="image.link"/>
+    <div v-if="isLoggedIn" class="image-container">
+        <img v-for="image in allImages" :src="image.link"/>
+    </div>
+    <h2 v-else>Please Log in to get started</h2>
   </div>
+  
 </template>
 
 <script>
@@ -9,10 +13,21 @@ import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'ImageList',
-  computed: mapGetters(['allImages']),
+  computed: mapGetters(['allImages', 'isLoggedIn']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages()
   }
 }
 </script>
+
+<style scoped>
+  .image-container {
+    column-count: 3;
+    column-gap: 0;
+  }
+  img {
+    max-width: 100%;
+    padding: 5px;
+  }
+</style>
