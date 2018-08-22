6949518f191ae7033a21b94ca9d1f58e88faa019
Thom David <thomdavid74@gmail.com>
2018-08-22 13:23:04 -0700

    L120/21 Test Imgur image fetch

--- image-browser/src/store/index.js
+++ image-browser/src/store/index.js
@@ -1,11 +1,13 @@
 import Vuex from 'vuex' // access to vuex
 import Vue from 'vue' // required to access vuex
 import auth from './modules/auth' // import auth module
+import images from './modules/images' // import images module
 
 Vue.use(Vuex) // allows Vue and Vuex to communicate
 
 export default new Vuex.Store({
   modules: {
-    auth // ES6 equivalent of auth: auth
+    auth, // ES6 equivalent of auth: auth
+    images // tell Vuex about the images module, === images: images
   }
 }) 
